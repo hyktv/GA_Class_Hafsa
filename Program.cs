d7b90b8053f47108c21f20a4476930f84f27854e
hyktv <hafsa.yktv@gmail.com>
2024-01-19 21:43:56 -0800

    finished

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace GA_Class_Hafsa
 {
+    //Hafsa Mohamed
+    //GA : Class
+    //01/19/24
     internal class Program
     {
         static void Main(string[] args)
@@ -7,6 +10,13 @@
             Console.WriteLine("Hello, World!");
             Student student = new Student();
             Student hafsa = new Student();
-        }
-    }
-}
+
+            Professor professor = new Professor();
+
+            Abaya abaya = new Abaya();
+
+        }//main
+
+    }//class
+
+}//namespace
