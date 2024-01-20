03a59912e14c7d7f5e9bae43a81d85a63ec8b203
hyktv <hafsa.yktv@gmail.com>
2024-01-19 21:38:33 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GA_Class_Hafsa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Student student = new Student();
+            Student hafsa = new Student();
+        }
+    }
+}
